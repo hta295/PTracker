86d1a285f8c1592eab209b10db65cd40916b707b
hta295 <tanner.haldeman@gmail.com>
2020-04-10 15:22:15 -0700

    Add/migrate model for collector
    Adds User and Interval models to db. Simple view at collector/index.html to demo progress.

--- collector/models.py
+++ collector/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
+
+class Interval(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_time = models.DateTimeField('starting time')
+    end_time = models.DateTimeField('ending time')
+
+    def compute_delta(self):
+        """Computes the time delta between the starting and ending time
+
+        :returns: a datetime timedelta object representing the time interval
+        """
+        return self.end_time - self.start_time
+
+    def __str__(self):
+        return f'{self.start_time} - {self.end_time}'
--- collector/views.py
+++ collector/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import User, Interval
+
+
+class IndexView(generic.ListView):
+    template_name = 'collector/index.html'
+
+    def get_queryset(self):
+        return User.objects.get(pk=1).interval_set.all().order_by('-start_time')[:5]
