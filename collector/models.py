c808a43c154f1250936d95b1af95670d694f3d3b
hta295 <tanner.haldeman@gmail.com>
2020-04-10 18:03:07 -0700

    Migrate from homegrown User model to native User model
    This allows us to leverage Django auth for Users.

--- collector/models.py
+++ collector/models.py
@@ -1,11 +1,6 @@
+from django.contrib.auth.models import User
+
 from django.db import models
-
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.username
 
 
 class Interval(models.Model):
