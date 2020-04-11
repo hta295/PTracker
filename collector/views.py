55a6db66ae14ec9c331dded55b5b13d33c5933f6
hta295 <tanner.haldeman@gmail.com>
2020-04-11 00:35:49 -0700

    Show the session user's intervals
    In the collector index we had hardcoded a test user's intervals be displayed; we now use the session's user. We redirect the user to the login page if not authenticated.

--- collector/views.py
+++ collector/views.py
@@ -1,16 +1,17 @@
 from dateutil import tz
 
-from django.views import generic
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
-from .models import User, Interval
 from .utils import convert_intervals_from_utc_to_localtime
 
 
-class IndexView(generic.ListView):
-    template_name = 'collector/index.html'
-
-    def get_queryset(self):
-        utc_intervals = User.objects.get(pk=1).interval_set.all().order_by('-start_time')
+def index(request):
+    if request.user.is_authenticated:
         # TODO: Get tz from the user request.
         # tzlocal() uses the TIME_ZONE from settings.py and not the end-user
-        return convert_intervals_from_utc_to_localtime(tz.tzlocal(), utc_intervals)
+        intervals = convert_intervals_from_utc_to_localtime(tz.tzlocal(),
+                        request.user.interval_set.all().order_by('-start_time'))
+        return render(request, 'collector/index.html', {'interval_list': intervals})
+    else:
+        return HttpResponseRedirect('../login')
