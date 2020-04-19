4b99e5edceaaab7f93c0ea5ec8d95c74eabb02bb
hta295 <tanner.haldeman@gmail.com>
2020-04-18 21:05:44 -0700

    Add logging to login/logout
    Simple logging configuration which prints DEBUG level messages to the console. Useful for debugging purposes.

--- productivity/views.py
+++ productivity/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as lin
 from django.contrib.auth import logout as lout
@@ -5,24 +7,33 @@ from django.http import HttpResponseRedirect, HttpResponse, HttpResponseBadReque
 from django.shortcuts import render
 from django.urls import reverse
 
+logger = logging.getLogger(__name__)
+
 
 def login(request):
+    ip = request.META.get('REMOTE_ADDR')
     if request.method == 'GET':
         return render(request, 'login/login.html')
     if 'username' not in request.POST or 'password' not in request.POST:
+        logger.debug(f"Remote host ({ip}) tried to POST to login without 'username' or 'password'")
         return HttpResponseBadRequest('"username" and "password" must be supplied in login request')
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(request, username=username, password=password)
     if user is not None:
         lin(request, user)
+        logger.debug(f"User ({username}) successfully logged in from remote host ({ip})")
         # Redirect to a success page.
         return HttpResponseRedirect(reverse('collector:index'))
     else:
+        logger.debug(f"User ({username}) failed to log in from remote host ({ip}) using invalid credentials")
         response = HttpResponse(status=401)
         return response
 
 
 def logout(request):
     lout(request)
+    ip = request.META.get('REMOTE_ADDR')
+    username = request.user.username if request.user.username else 'NOT AUTHENTICATED'
+    logger.debug(f"User ({username}) logged out of session from remote host ({ip})")
     return HttpResponseRedirect(reverse('collector:index'))
